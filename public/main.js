5cc350521bd807ffb1e35231d7a94debf89e307c
juthomas <juthomas@student.42.fr>
2022-03-26 02:04:50 +0100

    [ADD] map done

--- public/main.js
+++ public/main.js
@@ -5,70 +5,84 @@ document.body.appendChild(app.view);
 // Create the sprite and add it to the stage
 
 function createSquare(position) {
-  let square = new PIXI.Sprite(PIXI.Texture.WHITE);
-  square.position.set(position.x, position.y);
-  square.width = 10;
-  square.height = 10;
-  square.tint = '0x00FF00';
-  return (square);
+  let square = new PIXI.Sprite.from("/public/map_case.png");
+  square.position.set(position.x * 30, position.y * 30);
+  square.width = 30;
+  square.height = 30;
+  if (position.type == 1) square.tint = "0x00FF00";
+  return square;
 }
 
-let square = createSquare({x:10, y:10});
+let player = {x:0, y:0};
+
+let map = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+let itemsMap = map.map((row, y) => {
+  let currentRow = row.map((cell, x) => {
+    console.log("cell :", cell);
+    let currentCell = createSquare({ x: x, y: y, type: cell });
+    app.stage.addChild(currentCell);
+    return currentCell;
+  });
+  return currentRow;
+});
+
+console.log("Map :", itemsMap);
+
+let square = createSquare({ x: 10, y: 10 });
 let squareList = [];
 
 let direction = 0;
 
-for (let i = 0; i < 10; i++)
-{
-  squareList.push(createSquare({x:10*i, y:10}));
+for (let i = 0; i < 10; i++) {
+  squareList.push(createSquare({ x: 10 * i, y: 10 }));
 }
 // let sprite = PIXI.Sprite.from("/public/sample.png");
 // app.stage.addChild(sprite);
-for (let i = 0; i < 10; i++)
-{
+for (let i = 0; i < 10; i++) {
   app.stage.addChild(squareList[i]);
 }
 
-
 app.stage.addChild(square);
 
-document.addEventListener('keydown', (event) => {
-  var name = event.key;
-  var code = event.code;
-  // Alert the key name and key code on keydown
-  // alert(`Key pressed ${name} \r\n Key code value: ${code}`);
-  if (name == "ArrowRight")
-    direction = 0;
-  if (name == "ArrowLeft")
-    direction = 2;
-  if (name == "ArrowDown")
-    direction = 1;
-  if (name == "ArrowUp")
-    direction = 3;
-
-
-
-}, false);
+document.addEventListener(
+  "keydown",
+  (event) => {
+    var name = event.key;
+    var code = event.code;
+    // Alert the key name and key code on keydown
+    // alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+    if (name == "ArrowRight") direction = 0;
+    if (name == "ArrowLeft") direction = 2;
+    if (name == "ArrowDown") direction = 1;
+    if (name == "ArrowUp") direction = 3;
+  },
+  false
+);
 
 setInterval(() => {
-  
-  for (let i = 9; i >= 1; i--)
-  {
+  for (let i = 9; i >= 1; i--) {
     // console.log(squareList[i - 1]);
-    squareList[i].x = squareList[i - 1].x
-    squareList[i].y = squareList[i - 1].y
+    squareList[i].x = squareList[i - 1].x;
+    squareList[i].y = squareList[i - 1].y;
   }
 
-  if (direction == 0)
-    squareList[0].x = squareList[0].x + 10;
-    if (direction == 1)
-    squareList[0].y = squareList[0].y + 10;
-    if (direction == 2)
-    squareList[0].x = squareList[0].x - 10;
-    if (direction == 3)
-    squareList[0].y = squareList[0].y - 10;
-
-
+  if (direction == 0) squareList[0].x = squareList[0].x + 10;
+  if (direction == 1) squareList[0].y = squareList[0].y + 10;
+  if (direction == 2) squareList[0].x = squareList[0].x - 10;
+  if (direction == 3) squareList[0].y = squareList[0].y - 10;
 }, 200);
 
 // Add a ticker callback to move the sprite back and forth
