9aa536f13bc0b02ccaeda5c8a48f74990ce20142
Kévin RIFA <rifakev@gmail.com>
2022-03-26 21:40:36 +0000

    Revert "Merge branch 'kev_rifa' into 'master'"
    This reverts merge request !3

--- src/player-activities.js
+++
@@ -1,5 +0,0 @@
-function readActivitiesFile() {
-    document.getElementById("listActivities").innerHTML = '<object type="text/html" data="./src/activities-list.html" ></object>';
-}
-
-readActivitiesFile();
