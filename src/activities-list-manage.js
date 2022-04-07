4d85a4d8b2707b6b0b54cca7c648dcd1ba3923d8
juthomas <juthomas@student.42.fr>
2022-04-07 23:47:26 +0200

    [ADD] ...

--- src/activities-list-manage.js
+++ src/activities-list-manage.js
@@ -11,9 +11,22 @@
 
 // showActivitiesList('./src/activities-list.html');
 
+function createElementFromHTML() {
+  var div = document.createElement('div');
+  div.className = "notification"
+  div.innerHTML = '<div class="notification-img">Icon</div><div class="notification-text">A notification message..</div>';
+  setTimeout(() => {div.classList.add("show")}, 100);
+  setTimeout(() => {div.classList.remove("show")}, 10000);
+  // Change this to div.childNodes to support multiple top-level nodes.
+  return div;
+}
+
 // Écriture dans le document
 function writeActivity(textToAdd) {
   // Réception du document
-  document.getElementById("activitiesList").innerHTML += "<p>" + textToAdd + "</p>";
+  // document.getElementById("activitiesList").innerHTML += "<p>" + textToAdd + "</p>";
+  document.getElementById("activitiesList").appendChild(createElementFromHTML());
+
+
   console.log("actif");
 }
